41f162a8f811a6721d88b67a3cfbfb1deb138c7b
kayla <18447073684@163.com>
2024-11-16 12:47:26 +0800

    module：configuration（none）【配置中心】

--- oap-server/server-configuration/configuration-api/src/main/java/org/apache/skywalking/oap/server/configuration/api/NoneConfigurationProvider.java
+++ oap-server/server-configuration/configuration-api/src/main/java/org/apache/skywalking/oap/server/configuration/api/NoneConfigurationProvider.java
@@ -26,6 +26,9 @@ import org.apache.skywalking.oap.server.library.module.ServiceNotProvidedExcepti
 
 /**
  * A nutshell configuration implementor.
+ * <pre>
+ * (一个 nutshell 配置实现器。)
+ * </pre>
  */
 public class NoneConfigurationProvider extends ModuleProvider {
     @Override
@@ -35,20 +38,25 @@ public class NoneConfigurationProvider extends ModuleProvider {
 
     @Override
     public Class<? extends ModuleDefine> module() {
+        // 配置模块
         return ConfigurationModule.class;
     }
 
     @Override
     public ModuleConfig createConfigBeanIfAbsent() {
+        // 空的配置Bean
         return null;
     }
 
     @Override
     public void prepare() throws ServiceNotProvidedException, ModuleStartException {
+        // Provider 的 prepare 阶段：
+
+        // 为 "配置模块" 的 Provider（none） 注册 DynamicConfigurationService 的实现类
         this.registerServiceImplementation(DynamicConfigurationService.class, new DynamicConfigurationService() {
             @Override
             public void registerConfigChangeWatcher(ConfigChangeWatcher watcher) {
-
+                // 空实现
             }
         });
     }
